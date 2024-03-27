71d290349e579b4913e23da3e99b64d961663f48
kirankhanalleo <iamkirankhanal@gmail.com>
2024-03-27 18:57:22 +0545

    configured-data-source-and-physical-naming-strategy

---
+++ src/main/java/com/kirankhanal/HotelAPI/datasourceconfiguration.java
@@ -0,0 +1,34 @@
+package com.kirankhanal.HotelAPI;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class datasourceconfiguration {
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+    @Bean
+    public PhysicalNamingStrategyStandardImpl physicalNamingStrategy() {
+        return new PhysicalNamingStrategyStandardImpl();
+    }
+}
