7460513292512eddc4c26c834b1671e57e32a439
kirankhanalleo <iamkirankhanal@gmail.com>
2024-03-27 14:49:30 +0545

    added-hotel-controller-with-create-endpoint-and-integration-with-hotel-service

--- src/main/java/com/kirankhanal/HotelAPI/controller/hotelcontroller.java
+++ src/main/java/com/kirankhanal/HotelAPI/controller/hotelcontroller.java
@@ -1,9 +1,23 @@
 package com.kirankhanal.HotelAPI.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.kirankhanal.HotelAPI.repository.hotelrepository;
+import com.kirankhanal.HotelAPI.model.hotel;
+import com.kirankhanal.HotelAPI.service.hotelservice;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/hotels")
 public class hotelcontroller {
+    @Autowired
+    private hotelservice hotelservice;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createNewHotel(@RequestBody hotel hotel) {
+        hotelservice.createNewHotel(hotel);
+        return ResponseEntity.status(HttpStatus.OK).body("Hotel created successfully.");
+    }
 }
--- src/main/java/com/kirankhanal/HotelAPI/repository/hotelrepository.java
+++ src/main/java/com/kirankhanal/HotelAPI/repository/hotelrepository.java
@@ -2,5 +2,8 @@ package com.kirankhanal.HotelAPI.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.kirankhanal.HotelAPI.model.hotel;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface hotelrepository extends JpaRepository<hotel,Long> {
 }
