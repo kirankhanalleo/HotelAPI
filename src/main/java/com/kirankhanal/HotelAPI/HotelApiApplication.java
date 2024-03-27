56f3fd9df18c4f96ca3c870287abc37e823422ab
kirankhanalleo <iamkirankhanal@gmail.com>
2024-03-27 22:11:31 +0545

    added-swagger-ui-api-documentation

--- src/main/java/com/kirankhanal/HotelAPI/HotelApiApplication.java
+++ src/main/java/com/kirankhanal/HotelAPI/HotelApiApplication.java
@@ -1,9 +1,28 @@
 package com.kirankhanal.HotelAPI;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info=@Info(
+				title="Hotel Management System CRUD API Documentation",
+				version = "1.0.0",
+				description = "Welcome to the documentation for the HMS Rest APIs. This set of APIs" +
+						"allows developers to seamlessly integrate hotel management functionalities in their application. This " +
+						"documentation provides comprehensive guide through the process of integration these APIs for smooth communication" +
+						"between the system and the end users."
+		),
+		servers=@Server(
+				url="http://localhost:8080",
+				description="This server hosts the Hotel Management System APIs, providing endpoints for creating, updating, deleting, and retrieving hotel information."
+		)
+
+)
 public class HotelApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kirankhanal/HotelAPI/controller/hotelcontroller.java
+++ src/main/java/com/kirankhanal/HotelAPI/controller/hotelcontroller.java
@@ -1,9 +1,10 @@
 package com.kirankhanal.HotelAPI.controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.kirankhanal.HotelAPI.repository.hotelrepository;
 import com.kirankhanal.HotelAPI.model.hotel;
 import com.kirankhanal.HotelAPI.service.hotelservice;
 import java.util.ArrayList;
@@ -11,41 +12,70 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/hotels")
+@Tag(name="hotelcontroller",description = "Handles endpoints related to hotel management, including creating, updating, deleting, and retrieving hotel information.")
 public class hotelcontroller {
     @Autowired
     private hotelservice hotelservice;
-
+    @Operation(
+            summary = "Create a new hotel",
+            description = "This endpoint adds a new hotel to the database."
+    )
     @PostMapping("/create")
     public ResponseEntity<String> createNewHotel(@RequestBody hotel hotel) {
         hotelservice.createNewHotel(hotel);
         return ResponseEntity.status(HttpStatus.OK).body("Hotel created successfully.");
     }
+    @Operation(
+            summary = "Retrieve all hotels",
+            description = "This endpoint retrieves all hotels stored in the database."
+    )
     @GetMapping("/find")
     public ResponseEntity<List<hotel>> findAllHotels(){
         List<hotel> Hotels = new ArrayList<>(hotelservice.findAllHotels());
         return new ResponseEntity<>(Hotels,HttpStatus.OK);
     }
+    @Operation(
+            summary = "Retrieve hotel by ID",
+            description = "This endpoint fetches a hotel from the database based on its unique identifier."
+    )
     @GetMapping("/find/{id}")
     public ResponseEntity<hotel> findHotelById(@PathVariable Long id){
         Optional<hotel> Hotel=hotelservice.findHotelById(id);
         return Hotel.map(hotel -> new ResponseEntity<>(hotel, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+    @Operation(
+            summary = "Search for hotels by location",
+            description = "This endpoint find hotels based on the specified location parameter"
+    )
     @GetMapping("/search")
     public ResponseEntity<List<hotel>> findHotelByLocation(@RequestParam String location){
         List<hotel> Hotels = new ArrayList<>(hotelservice.findHotelByLocation(location));
         return new ResponseEntity<>(Hotels,HttpStatus.OK);
     }
+    @Operation(
+            summary = "Update hotel by ID",
+            description = "This endpoint updates a hotel from the database based on its unique identifier."
+    )
     @PatchMapping("/update/{id}")
     public ResponseEntity<hotel> updateHotel(@PathVariable Long id, @RequestBody hotel updatedHotel){
         hotel updated = hotelservice.updateHotel(id,updatedHotel);
         return new ResponseEntity<>(updated,HttpStatus.OK);
     }
+    @Operation(
+            summary = "Delete hotel by ID",
+            description = "This endpoint deletes a hotel from the database based on its unique identifier."
+    )
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteHotelById(@PathVariable Long id){
         hotelservice.deleteHotelById(id);
         return ResponseEntity.status(HttpStatus.OK).body("Hotel with id "+id+" deleted successfully");
     }
+    @Operation(
+            summary = "Delete all hotels",
+            description = "This endpoint deletes all hotels from the database."
+    )
     @DeleteMapping("/delete/all")
     public ResponseEntity<String> deleteAllHotel(){
         hotelservice.deleteAllHotels();
