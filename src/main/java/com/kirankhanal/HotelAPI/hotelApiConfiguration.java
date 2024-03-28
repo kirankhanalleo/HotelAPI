b5b1c74906faadc7fcf018eecc62e4df5fd08a3d
kirankhanalleo <iamkirankhanal@gmail.com>
2024-03-28 13:59:57 +0545

    implemented-spring-security-for-api-validation

--- src/main/java/com/kirankhanal/HotelAPI/hotelApiConfiguration.java
+++ src/main/java/com/kirankhanal/HotelAPI/hotelApiConfiguration.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import javax.sql.DataSource;
 
 @Configuration
-public class datasourceconfiguration {
+@EnableWebSecurity
+public class hotelApiConfiguration {
     @Value("${spring.datasource.url}")
     private String url;
 
@@ -17,6 +23,11 @@ public class datasourceconfiguration {
 
     @Value("${spring.datasource.password}")
     private String password;
+
+    @Value("${spring.security.user.name}")
+    private String loginusername;
+    @Value("${spring.security.user.password}")
+    private String loginpassword;
 
     @Bean
     public DataSource dataSource(){
@@ -30,5 +41,14 @@ public class datasourceconfiguration {
     @Bean
     public PhysicalNamingStrategyStandardImpl physicalNamingStrategy() {
         return new PhysicalNamingStrategyStandardImpl();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails user = User.builder().username(loginusername).password(passwordEncoder().encode(loginpassword)).roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
